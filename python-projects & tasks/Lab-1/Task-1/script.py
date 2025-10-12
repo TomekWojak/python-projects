c2435126db14ce97b2144cd3d552952b8093e96d
Tomek <tomek.wojak796@gmail.com>
2025-10-12 21:33:20 +0200

    I completed example B from the first task

--- python-projects & tasks/Lab-1/Task-1/script.py
+++ python-projects & tasks/Lab-1/Task-1/script.py
@@ -30,6 +30,17 @@ print(type(8 ** (1/3))) # 2.0 <class 'float'> ** => operator potęgowania
 
 # Przykład B
 
+#1
+print(int(3.0), type(int(3.0))) # 3, <class 'int'> => int wymusza konwersję na liczbę całkowtą
 
+#2
+print(float(3), type(float(float(3)))) # 3.0, <class 'float'> => float wymusza konwersję na liczbę zmiennoprzecinkową
 
+#3
+print(float("3"), type(float(float(3)))) # 3.0, <class 'float'> => konwersja str na liczbę zmiennoprzecinkową
 
+#4
+print(str(12.4), type(str(12.4))) # "12.4", <class 'str'> => konwersja liczby zmiennoprzecinkowej na str
+
+#5
+print(bool(0), type(bool(0))) # False, <class 'bool'> => konwersja na typ bool, cyfra zero należy do 'falsy values', więc wynik to False
