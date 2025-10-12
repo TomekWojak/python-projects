b40f70965aa8ad6655dba7e70b9c684c1d868235
Tomek <tomek.wojak796@gmail.com>
2025-10-12 21:50:26 +0200

    I did the second and third exercises

---
+++ python-projects & tasks/Lab-1/Task-3/script.py
@@ -0,0 +1,12 @@
+imię = 'Jan'
+wiek = 20
+wzrost = 178
+
+print('Nazywam się' + " " + imię + " " + "i mam" + " " + str(wiek) + " lat" + '\n' + "Mój wzrost to" + " " + str(wzrost) + " " + "cm")
+# konkatenacja
+
+print(f"Nazywam się {imię} i mam {wiek} lat \nMój wzrost to {wzrost} cm")
+# f-string
+
+# Nazywam się Jan i mam 20 lat.
+# Mój wzrost to 178 cm.
