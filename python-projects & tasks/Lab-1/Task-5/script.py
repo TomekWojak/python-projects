8a9d5fa12d99f69cf3536b951f6342802e9ad568
Tomek <tomek.wojak796@gmail.com>
2025-10-12 22:32:08 +0200

    Completed the task five

---
+++ python-projects & tasks/Lab-1/Task-5/script.py
@@ -0,0 +1,7 @@
+a = int(input('Podaj bok a: '))
+b = int(input('Podaj bok b: '))
+
+pole_prostokota = a * b
+obwod_prostokota = 2 * a + 2 * b
+
+print(f'Pole prostokąta wynosi: {pole_prostokota} \nObwód prostokąta wynosi: {obwod_prostokota}')
