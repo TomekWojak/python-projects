b18e0aff2f5a6da86a95b41208005a6dad54424b
Tomek <tomek.wojak796@gmail.com>
2025-10-12 20:44:36 +0200

    I did example A from task one

---
+++ python-projects & tasks/Lab-1/Task-1/script.py
@@ -0,0 +1,35 @@
+
+# Przykład A
+# 1.
+print(type(1 + 2)) # 3, <class 'int'>, + => operator dodawania
+
+#2
+print(type(1 + 4.5)) # 5.5, <class 'float'>, + => operator dodawania
+
+#3
+print(type(3 / 2)) # 1.5, <class 'float'>, / => operator dzielenia
+
+#4
+print(type(4 / 2)) # 2.0, <class 'float'>, / => operator dzielenia
+
+#5
+print(type(3 // 2)) # 1  <class 'int'> // => operator dzielenia całkowitego z zaokrąglaniem w dół
+
+#6
+print(type(-3 // 2)) # -2, <class 'int'> // => operator dzielenia całkowitego z zaokrąglaniem w dół
+
+#7
+print(type(11 % 2 )) # 1, <class 'int'> % => modulo, określa resztę z dzielenia przez daną liczbę
+
+#8
+print(type(2 ** 10)) # 1024, <class 'int'> ** => operator potęgowania
+
+#9
+print(type(8 ** (1/3))) # 2.0 <class 'float'> ** => operator potęgowania
+
+
+# Przykład B
+
+
+
+
