3d491adc6f952e07d81e1efa81a93f5b3b9d1f6b
Tomek <tomek.wojak796@gmail.com>
2025-10-26 21:08:11 +0100

    updated the functionality of the function

--- Python-projekty i zadania/Lab-1/Zadanie-6/script.py
+++ Python-projekty i zadania/Lab-1/Zadanie-6/script.py
@@ -8,14 +8,14 @@ help(random.randint) # randint(a, b) method of random.Random instance, Return ra
 def oblicz_koszt_podróży():
     droga_losowa = random.randint(10, 100)
 
-    przebyta_droga = int(input('Podaj przebytą drogę: ')  or droga_losowa)
-    srednie_spalanie = int(input('Podaj średnie spalanie: '))
-    cena_paliwa = int(input('Podaj aktualną cenę paliwa: ') or CENA_PALIWA)
+    przebyta_droga = float(input('Podaj przebytą drogę: ')  or droga_losowa)
+    srednie_spalanie = float(input('Podaj średnie spalanie: ') or 0)
+    cena_paliwa = float(input('Podaj aktualną cenę paliwa: ') or CENA_PALIWA)
 
     zuzycie_paliwa = przebyta_droga * srednie_spalanie / JEDNOSTKA_ZUZYCIA
     przewidywany_koszt = cena_paliwa * zuzycie_paliwa
 
-    print(f'Zużycie paliwa wynosi: {zuzycie_paliwa} litrów, a koszty szacowane są na {round(przewidywany_koszt, 2)} zł')
+    print(f'Dla przebytej drogi: {przebyta_droga}km, zużycie paliwa wynosi: {zuzycie_paliwa} litrów, a koszty szacowane są na {round(przewidywany_koszt, 2)} zł')
 
 oblicz_koszt_podróży()
 
