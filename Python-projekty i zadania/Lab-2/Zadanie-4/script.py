011bff5e62442470873dbe4231ad30b6efb70d47
Tomek <tomek.wojak796@gmail.com>
2025-10-28 00:23:15 +0100

    Completed task 4

--- Python-projekty i zadania/Lab-2/Zadanie-4/script.py
+++ Python-projekty i zadania/Lab-2/Zadanie-4/script.py
@@ -1,4 +1,8 @@
-def obsluzWalidacje():
+PUNKTY_ZA_BRAMKE = 10
+BONUS_ZA_5_BRAMEK = 5
+BONUS_ZA_10_BRAMEK = 10
+
+def obsluzFunkcjeMenedzeraFootbolu():
     while True:
         gol = input('Podaj liczbę strzelonych bramek: ')
 
@@ -13,5 +17,18 @@ def obsluzWalidacje():
                 print('Podano niepoprawne dane!')
 
 
+def obliczWynikDruzyny(gol):
+    liczbaBramek = int(gol)
+    wynikZaBramki = liczbaBramek * PUNKTY_ZA_BRAMKE
+    bonus = 0
 
+    if liczbaBramek > 10:
+        bonus = BONUS_ZA_5_BRAMEK + BONUS_ZA_10_BRAMEK
+    elif liczbaBramek > 5:
+        bonus = BONUS_ZA_5_BRAMEK
 
+    calkowityWynik = wynikZaBramki + bonus
+
+    print(f'Drużyna uzyskała {wynikZaBramki} punktów za strzelone gole \nCałkowity wynik drużyny wynosi {calkowityWynik} punktów')
+
+obsluzFunkcjeMenedzeraFootbolu()
