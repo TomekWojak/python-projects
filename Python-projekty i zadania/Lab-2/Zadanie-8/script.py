040e899716d5f9827a84ef7614a19ea01c1e8db9
Tomek <tomek.wojak796@gmail.com>
2025-10-28 15:13:30 +0100

    Completed task 8

---
+++ Python-projekty i zadania/Lab-2/Zadanie-8/script.py
@@ -0,0 +1,10 @@
+text = 'Studiuje-Informatykę'
+
+def wyodrebnijZnaki():
+    pierwszeTrzyZnaki = text[0:3]
+    ostatnieDwaZnaki = text[-2:]
+
+    print(f'Pierwsze trzy znaki wyrazu {text} to {pierwszeTrzyZnaki}')
+    print(f'Ostatnie dwa znaki wyrazu {text} to {ostatnieDwaZnaki}')
+
+wyodrebnijZnaki()
