87c5d2147af4db1cf9f0842c7070702e12029b27
Tomek <tomek.wojak796@gmail.com>
2025-10-28 14:30:21 +0100

    completed task 6

---
+++ Python-projekty i zadania/Lab-2/Zadanie-6/script.py
@@ -0,0 +1,22 @@
+def sprawdzWielkoscLitery():
+    while True:
+        litera = input('Podaj literę lub wyraz: ').strip()
+
+        if not litera:
+            continue
+        else: break
+
+    if len(litera) == 1:
+        if litera == litera.upper():
+            print(f'Litera {litera} jest duża')
+        else:
+            print(f'Litera {litera} jest mała')
+    else:
+        if litera[-1] == litera[-1].upper():
+            print(f'Ostatnia litera podanego wyrazu - {litera}, czyli {litera[-1]} jest duża')
+        else:
+            print(f'Ostatnia litera podanego wyrazu - {litera}, czyli {litera[-1]} jest mała')
+
+sprawdzWielkoscLitery()
+
+# Program sprawdza wielkość litery podanej przez użytkownika, a w przypadku wpisania wyrazu - poda wielkość ostaniej litery
