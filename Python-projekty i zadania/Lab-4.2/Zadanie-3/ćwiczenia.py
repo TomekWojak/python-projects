24b0ab23e300f30e802da1bdb279175bd824ba78
Tomek <tomek.wojak796@gmail.com>
2025-12-06 00:13:29 +0100

    Completed task 3

---
+++ Python-projekty i zadania/Lab-4.2/Zadanie-3/ćwiczenia.py
@@ -0,0 +1,6 @@
+import f_mat
+from f_mat import kwadrat, dodaj, szescian
+
+print(f'Kwadrat liczby 10 wynosi: {kwadrat(10)}')
+print(f'Sześcian liczby 3 wynosi: {szescian(3)}')
+print(f'Suma liczb 10 i 5 wynosi: {dodaj(10,5)}')
