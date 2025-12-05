53860e6ed2de43698864f6d45dd430838eae66d4
Tomek <tomek.wojak796@gmail.com>
2025-12-05 14:03:53 +0100

    Completed task 4

--- Python-projekty i zadania/Lab-4.1/Zadanie-4/script.py
+++ Python-projekty i zadania/Lab-4.1/Zadanie-4/script.py
@@ -1,6 +1,8 @@
 def obliczSrednia(*liczby):
     srednia = sum(liczby) / len(liczby) if len(liczby) != 0 else None
 
+    if not liczby: return "Brak danych do obliczeń"
+
     return srednia
 
 print(obliczSrednia(1,2,3,4,5))
