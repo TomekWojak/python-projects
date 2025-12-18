3306dc8f640f6ec5d5f8862a0cd3f01616d335f3
Tomek <tomek.wojak796@gmail.com>
2025-12-18 19:59:18 +0100

    Completed task 2

---
+++ Python-projekty i zadania/Lab-5/Zadanie-2/script.py
@@ -0,0 +1,12 @@
+import pandas as pd
+df = pd.read_csv(
+    "../demografia.csv",
+    decimal=",",
+    na_values=["", "NA", "brak"]
+)
+dane_2022 = df["2022"]
+max_przyrost = dane_2022.idxmax()
+kraj = df.loc[max_przyrost, "KRAJE"]
+print(f'Kraj, w którym odnotowano największy przyrost ludności to {kraj}')
+
+
