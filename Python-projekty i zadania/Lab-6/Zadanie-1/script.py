f6896387e239812b5d9bf67a87aed13268323843
Tomek <tomek.wojak796@gmail.com>
2026-01-24 21:56:31 +0100

    Finished task 1

---
+++ Python-projekty i zadania/Lab-6/Zadanie-1/script.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+kategorie = ['Pieczywo', 'Słodycze', 'Nabiał', 'Napoje', 'Mięso']
+wartosci = [150, 290, 320, 220, 400]
+plt.bar(kategorie, wartosci)
+plt.show()
