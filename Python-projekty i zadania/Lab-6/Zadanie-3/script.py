cfab5e51c1a6b46a257aba5ca9a92c90a931f275
Tomek <tomek.wojak796@gmail.com>
2026-01-24 22:24:00 +0100

    Finished task 3

---
+++ Python-projekty i zadania/Lab-6/Zadanie-3/script.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# czas (s)
+x = [1, 2, 3, 4, 5]
+
+# prędkość chwilowa (m/s)
+y = [2, 4, 6, 8, 10]
+
+plt.scatter(x, y)
+plt.xlabel("Czas [s]")
+plt.ylabel("Prędkość chwilowa [m/s]")
+plt.title("Zależność prędkości od czasu")
+
+plt.show()
