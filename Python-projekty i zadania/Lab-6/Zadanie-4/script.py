4efd4d343b6fb8c7c2b33d4a549564ab9d960db4
Tomek <tomek.wojak796@gmail.com>
2026-01-24 22:54:17 +0100

    Finished task 4

---
+++ Python-projekty i zadania/Lab-6/Zadanie-4/script.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+lista_wagi = [128, 64, 32, 16, 8, 4, 2, 1]
+wagi = np.array(lista_wagi)
+liczba_bin = np.random.randint(0, 2, 8)
+
+def wartosc_liczby_bin(wagi, liczba_bin):
+    return np.sum(wagi * liczba_bin)
+
+print("Wagi:", wagi)
+print("Liczba binarna:", liczba_bin)
+print("Wartość dziesiętna:", wartosc_liczby_bin(wagi, liczba_bin))
