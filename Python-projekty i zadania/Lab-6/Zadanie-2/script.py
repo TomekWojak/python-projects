7bd13fab43394577e7d2b1ad69275cef48d5d4fa
Tomek <tomek.wojak796@gmail.com>
2026-01-24 22:13:52 +0100

    Finished task 2

---
+++ Python-projekty i zadania/Lab-6/Zadanie-2/script.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+kategorie = ['Pieczywo', 'Słodycze', 'Nabiał', 'Napoje', 'Mięso']
+udzial = [150, 290, 320, 220, 400]
+plt.pie(udzial, labels=kategorie,
+autopct='%1.f%%', startangle=90,
+colors=['skyblue', 'lightgreen',
+'lightcoral', 'gold'])
+plt.title('Udział w kategoriach')
+plt.show()
